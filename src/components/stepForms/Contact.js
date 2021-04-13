a948bfc48e204446f8423c92bab8918dba5a16c0
allampally jaipal david tejesvi <davidtejusvi@Davids-MacBook.local>
2021-04-13 13:20:03 +0530

    fix the editable fields

--- src/components/stepForms/Contact.js
+++ src/components/stepForms/Contact.js
@@ -1,60 +1,51 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-export const Contact = ({formData, setForm, navigation}) => {
-    const {phone, email } = formData;
-    return (
-        <Container maxWidth="xs">
-            <h3>Contact</h3>
+export const Contact = ({ formData, setForm, navigation }) => {
+  const { phone, email } = formData;
 
-            { /* Phone field*/}
-
-            <TextField 
-            label = "Phone"
-            value = "phone"
-            value = {phone}
-            onChange={setForm}
-            margin="normal"
-            variant="outlined"
-            autoComplete="off"
-            fullWidth/>
-
-            {/*email field*/}
-
-            <TextField 
-            label = "Email"
-            value = "email"
-            value = {email}
-            onChange={setForm}
-            margin="normal"
-            variant="outlined"
-            autoComplete="off"
-            fullWidth/>
-
-            {/* Button field*/}
-            
-            <div style={{ marginTop: '1rem'}}>
-                <Button 
-                variant="contained" 
-                color="secondary" 
-                style={{marginRight :"1rem"}}
-                onClick={()=>navigation.previous()}
-                >
-                Back
-                </Button>
-
-                <Button 
-                variant="contained" 
-                color="primary" 
-                
-                onClick={()=>navigation.next()}
-                >
-                Next
-                </Button>
-            </div>
-            
-        </Container>
-    )
-}
+  return (
+    <Container maxWidth="xs">
+      <h3>Contact</h3>
+      <TextField
+        label="Phone"
+        name="phone"
+        value={phone}
+        onChange={setForm}
+        margin="normal"
+        variant="outlined"
+        autoComplete="off"
+        fullWidth
+      />
+      <TextField
+        label="E-Mail"
+        name="email"
+        value={email}
+        onChange={setForm}
+        margin="normal"
+        variant="outlined"
+        autoComplete="off"
+        fullWidth
+      />
+      <div style={{ marginTop: "1rem" }}>
+        <Button
+          color="secondary"
+          variant="contained"
+          style={{ marginRight: "1rem" }}
+          onClick={() => navigation.previous()}
+        >
+          Back
+        </Button>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={() => navigation.next()}
+        >
+          Next
+        </Button>
+      </div>
+    </Container>
+  );
+};
--- src/components/stepForms/Names.js
+++ src/components/stepForms/Names.js
@@ -1,66 +1,53 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
+export const Names = ({ formData, setForm, navigation }) => {
+  const { firstName, lastName, nickName } = formData;
 
-export const Names = ({formData, setForm, navigation}) => {
-    const {firstName, lastName, nickName } = formData;
-    return (
-
-        <Container maxWidth="xs">
-            <h3>Names</h3>
-           {/* Firstname field*/}
-            <TextField 
-            label = "First Name"
-            value = "firstName"
-            value = {firstName}
-            onChange={setForm}
-            margin="normal"
-            variant="outlined"
-            autoComplete="off"
-            fullWidth/>
-
-            {/* Last Name field*/}
-
-            <TextField 
-            label = "Last Name"
-            value = "lastName"
-            value = {lastName}
-            onChange={setForm}
-            margin="normal"
-            variant="outlined"
-            autoComplete="off"
-            fullWidth/>
-
-            {/* Nick Name field*/}
-
-            <TextField 
-            label = "Nick Name"
-            value = "nickName"
-            value = {nickName}
-            onChange={setForm}
-            margin="normal"
-            variant="outlined"
-            autoComplete="off"
-            fullWidth/>
-
-            {/* Button */}
-            
-            <Button 
-                variant="contained" 
-                fullWidth 
-                color="primary" 
-                style={{marginTop :"1rem"}}
-                onClick={()=>navigation.next()}
-            >
-                Next
-            </Button>
-
-            
-        </Container>
-    )
-}
-
-
-
+  return (
+    <Container maxWidth="xs">
+      <h3>Names</h3>
+      <TextField
+        label="First Name"
+        name="firstName"
+        value={firstName}
+        onChange={setForm}
+        margin="normal"
+        variant="outlined"
+        autoComplete="off"
+        fullWidth
+      />
+      <TextField
+        label="Last Name"
+        name="lastName"
+        value={lastName}
+        onChange={setForm}
+        margin="normal"
+        variant="outlined"
+        autoComplete="off"
+        fullWidth
+      />
+      <TextField
+        label="Nick Name"
+        name="nickName"
+        value={nickName}
+        onChange={setForm}
+        margin="normal"
+        variant="outlined"
+        autoComplete="off"
+        fullWidth
+      />
+      <Button
+        variant="contained"
+        fullWidth
+        color="primary"
+        style={{ marginTop: "1rem" }}
+        onClick={() => navigation.next()}
+      >
+        Next
+      </Button>
+    </Container>
+  );
+};
